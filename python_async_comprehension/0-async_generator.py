5b1878ca3b7b2dd2834782ca1f643bb75204d4a2
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-05-02 13:58:08 +0400

    mesure time

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,6 +7,6 @@ from random import random
 
 async def async_generator() -> Generator[float, None, None]:
     """Async generator"""
-    await asyncio.sleep(1)
     for _ in range(10):
+        await asyncio.sleep(1)
         yield random() * 10
