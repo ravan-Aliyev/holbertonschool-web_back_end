d22c6dabc9031a0492e4f1c05224aa464aa65b1b
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-05-08 15:54:31 +0400

    fixing comment

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """list all values"""
 
 
 def insert_school(mongo_collection, **kwargs):
     """list all values"""
-    return mongo_collection.insert_one(kwargs)
+    return mongo_collection.insert(kwargs)
