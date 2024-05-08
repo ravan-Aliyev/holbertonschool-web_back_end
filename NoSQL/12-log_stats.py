b8bcdc06acbf9f655947ee615c075ae38e3fb50d
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-05-08 17:04:59 +0400

    fixing code

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -7,15 +7,15 @@ if __name__ == "__main__":
     """Find things"""
     client = MongoClient('mongodb://127.0.0.1:27017')
     logs_collection = client.logs.nginx
-    length = len(list(logs_collection.find()))
+    length = logs_collection.count_documents({})
     arr = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
     print("{0} logs".format(length))
     print("Methods:")
     for method in arr:
-        lengths = len(list(logs_collection.find({"method": method})))
+        lengths = logs_collection.count_documents({"method": method})
         print("\tmethod {0}: {1}".format(method, lengths))
 
-    status = len(list(logs_collection.find({"method": "GET",
-                                            "path": "/status"})))
+    status = logs_collection.count_documents({"method": "GET",
+                                            "path": "/status"})
     print("{0} status check".format(status))
