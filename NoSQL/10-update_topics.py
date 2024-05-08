a7da9bdd29bc054d40c49ddfa9bad51a12054edd
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-05-08 16:03:21 +0400

    change school topics

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""list all values"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """list all values"""
+    myquery = {"name": name}
+    newvalues = {"$set": {"topics": topics}}
+    return mongo_collection.update_one(myquery, newvalues)
