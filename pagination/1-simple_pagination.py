3330ad1ca78525972d7e29abd0c758f18570c774
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-05-10 19:56:57 +0400

    simple pagination

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,42 @@
+import csv
+import math
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Getting page
+        """
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
+
+        indexes = index_range(page, page_size)
+        s, e = indexes[0], indexes[1]
+
+        try:
+            return self.dataset()[s:e]
+        except IndexError:
+            return []
+    
+
+def index_range(page, page_size):
+    """Find page size"""
+    return (page_size * (page - 1), page_size * page)
