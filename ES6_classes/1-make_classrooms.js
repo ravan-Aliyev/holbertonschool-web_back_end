5debc31dcb7e69ba1cbab868ae7cdb3ca82dedfd
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-05-15 12:35:30 +0400

    make classrooms

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-vars */
+// eslint-disable-next-line import/extensions
+import ClassRoom from "./0-classroom";
+
+export default function initializeRooms() {
+  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+}
