8af1c81bb8a86aab339bb50e53f379d8a9671286
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-05-19 17:19:12 +0400

    airport

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
+}
