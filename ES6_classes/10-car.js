9cb2bef59f7a4c8af02eac7768600bd143729504
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-05-19 17:22:06 +0400

    car

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    const newCar = this.constructor[Symbol.species] || this.constructor;
+    const clone = new newCar();
+    return clone;
+  }
+}
