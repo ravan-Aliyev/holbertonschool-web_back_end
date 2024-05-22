268ac74c6e5a28b9bccff497da87f3571b892de4
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-05-22 12:06:15 +0400

    multiple promise

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+/* eslint disable */
+
+import { uploadPhoto, createUser } from "./utils";
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((val) => {
+      const [photo, user] = val;
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    })
+    .catch(() => console.log("Signup system offline"));
+}
