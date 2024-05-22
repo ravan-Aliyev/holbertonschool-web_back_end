f346d95c25fe1b9eebef5fa516ebff10810549db
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-05-22 16:24:02 +0400

    groceries list

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+
+export default function groceriesLis() {
+  const fruits = new Map([
+    ["Apples", 10],
+    ["Tomatoes", 10],
+    ["Pasta", 1],
+    ["Rice", 1],
+    ["Banana", 5],
+  ]);
+
+  return fruits;
+}
