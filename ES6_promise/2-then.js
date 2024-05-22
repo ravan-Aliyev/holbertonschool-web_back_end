f2ad3ff651c5566e17bd827a68d621e09f7e87bc
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-05-22 11:49:38 +0400

    fixing typeo

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -2,7 +2,7 @@
 
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then((val) => ({ status: 200, body: "Success" }))
+    .then(() => ({ status: 200, body: "success" }))
     .catch(() => new Error())
     .finally(() => console.log("Got a response from the API"));
 }
