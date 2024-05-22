456c75c72b00b6dd3540be8be0d27ee44fc4f96b
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-05-22 12:22:49 +0400

    handle multiple promise

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((val) => {
+    val.forEach((item) => ({
+      status: item.status,
+      value: item.value,
+    }));
+  });
+}
