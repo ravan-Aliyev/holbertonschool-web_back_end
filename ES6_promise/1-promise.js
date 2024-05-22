b9bc47ebda55067023bdec3eb14500a7449f87ef
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-05-22 11:17:44 +0400

    promise 2

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -5,7 +5,7 @@ export default function getFullResponseFromAPI(success) {
     if (success)
       resolve({
         status: 200,
-        body: "Succes",
+        body: "Success",
       });
     else {
       reject(new Error("The fake API is not working currently"));
