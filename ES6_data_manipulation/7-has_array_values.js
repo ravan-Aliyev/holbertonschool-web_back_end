141de54c49e07582a7f628fecf722c7bd09f6359
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-05-22 15:38:57 +0400

    more set

---
+++ ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+
+export default function hasValuesFromArray(set, array) {
+  for (const item of array) {
+    if (!set.has(item)) {
+      return false;
+    }
+  }
+
+  return true;
+}
