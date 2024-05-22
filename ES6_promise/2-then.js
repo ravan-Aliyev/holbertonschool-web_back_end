166d44effb8011e271a9859f4f0db87aa3460792
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-05-22 11:30:19 +0400

    then

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 
 export default function handleResponseFromAPI(promise) {
-  promise
+  return promise
     .then((val) => ({ status: 200, body: "Success" }))
     .catch(() => new Error())
     .finally(() => console.log("Got a response from the API"));
